46cc49316ed6b518c7ab2b12573bbed456ed130c
Anony <167723839+andrielcf@users.noreply.github.com>
2025-07-08 18:58:26 -0300

    feat(email-sender): Adicionando botão clicavel para usuario alterar senha

--- src/main/java/com/nutrieplan/nutrieplan/services/PasswordResetTokenService.java
+++ src/main/java/com/nutrieplan/nutrieplan/services/PasswordResetTokenService.java
@@ -7,12 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import com.nutrieplan.nutrieplan.entity.user.User;
 import com.nutrieplan.nutrieplan.entity.user.PasswordReset.PasswordResetToken;
 import com.nutrieplan.nutrieplan.repositories.PasswordResetTokenRepository;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import jakarta.transaction.Transactional;
 
 @Service
@@ -27,7 +30,7 @@ public class PasswordResetTokenService {
     private long expiryTime;
 
     @Transactional
-    public void createPasswordResetTokenForUser(User user) {
+    public void createPasswordResetTokenForUser(User user) throws MessagingException {
         // Remove qualquer token antigo desse usuário
         tokenRepository.deleteByUser(user);
 
@@ -48,7 +51,7 @@ public class PasswordResetTokenService {
         myToken.setExpiryDate(new Date(System.currentTimeMillis() + expiryTime));
 
         tokenRepository.save(myToken);
-        
+
         String resetLink = "http://localhost:5173/reset-password/" + token;
         sendPasswordResetEmail(user.getEmail(), resetLink);
     }
@@ -70,13 +73,31 @@ public class PasswordResetTokenService {
         return tokenRepository.findByToken(token).getUser();
     }
 
-    private void sendPasswordResetEmail(String email, String resetLink) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("nutrieplan@gmail.com");
-        message.setTo(email);
-        message.setSubject("Redefinição de senha - NutriePlan");
-        message.setText("Para redefinir sua senha, clique no link abaixo:\n" + resetLink +
-                "\n\nSe você não solicitou esta redefinição, ignore este e-mail.");
-        mailSender.send(message);
+    // private void sendPasswordResetEmail(String email, String resetLink) {
+    // SimpleMailMessage message = new SimpleMailMessage();
+    // message.setFrom("nutrieplan@gmail.com");
+    // message.setTo(email);
+    // message.setSubject("Redefinição de senha - NutriePlan");
+    // message.setText("Para redefinir sua senha, clique no link abaixo:\n" +
+    // resetLink +
+    // "\n\nSe você não solicitou esta redefinição, ignore este e-mail.");
+    // mailSender.send(message);
+    // }
+    private void sendPasswordResetEmail(String email, String resetLink) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+
+        String htmlContent = "<p>Para redefinir sua senha, clique no botão abaixo:</p>"
+                + "<a href=\"" + resetLink + "\" style=\"display:inline-block;padding:10px 20px;"
+                + "font-size:16px;color:#fff;background-color:#28a745;text-decoration:none;"
+                + "border-radius:5px;\">Redefinir Senha</a>"
+                + "<p>Se você não solicitou esta redefinição, ignore este e-mail.</p>";
+
+        helper.setFrom("nutrieplan@gmail.com");
+        helper.setTo(email);
+        helper.setSubject("Redefinição de senha - NutriePlan");
+        helper.setText(htmlContent, true); // O segundo parâmetro indica que é HTML
+
+        mailSender.send(mimeMessage);
     }
 }
